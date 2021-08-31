6b18cf526016ff157c14801711ccd7b4446b88b4
Nicholas Barbosa <47508677+johnpotato359@users.noreply.github.com>
2021-08-31 14:12:27 -0300

    Aula 14

---
+++ aula14/script.js
@@ -0,0 +1,13 @@
+var contador = 0;
+
+while(contador <= 10) {
+    console.log(`Hello, Wolrd [${contador}]`);
+    contador++;
+}
+
+contador = 0;
+
+do {
+    console.log(`Hello again [${contador}]`);
+    contador++;
+} while(contador <= 10)
