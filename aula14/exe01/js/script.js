9d3e2001b23af9a09b563be4997b121502f56b36
Nicholas Barbosa <47508677+johnpotato359@users.noreply.github.com>
2021-09-01 17:49:41 -0300

    Aula 14 - Exercicios
    Correcao do Exercicio 1 da Aula 14

--- aula14/exe01/js/script.js
+++ aula14/exe01/js/script.js
@@ -8,22 +8,22 @@ function contarNumeros() {
     let fim = Number(valorFim.value);
     let passo = Number(valorPasso.value);
 
-    if(valorInicio.value.length == 0 || valorFim.value.length == 0 
-        || valorPasso.value.length == 0) {
-            alert('[ERRO] Dados incompletos ou incorretos!');
-        }
-    else {
-        resultado.innerHTML = 'Contando...';
-        if(inicio >= fim) {
-            for(let i = inicio; i <= fim; i += passo) {
-                resultado.innerHTML += ` ${i} \u{1F449}`;
-            }
+    if(valorInicio.value.length == 0 
+        || valorFim.value.length == 0
+        || valorPasso.value.length == 0){
+            alert('[ERRO] Faltam dados!');
         } else {
-            for(let i = fim; i <= inicio; i -= passo) {
-                resultado.innerHTML += ` ${i} \u{1F449}`;
-                console.log(i);
+            resultado.innerHTML = 'Contando...';
+            if(inicio < fim){
+                for(let i = inicio; i <= fim; i+= passo){
+                    resultado.innerHTML += `${i} \u{1F449}`;
+                }
+            } else {
+                for(let i = inicio; i >= fim; i -= passo) {
+                    resultado.innerHTML += `${i} \u{1F449}`;
+                }
             }
+            resultado.innerHTML += `\u{1F3C1}`;
         }
-        resultado.innerHTML += `\u{1F3C1}`;
-    }
 }
+
